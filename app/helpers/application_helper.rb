15f7ed4caab08d06598d22c7e14786d36b0a51bb
Agnė Klimaitė <agnaite@me.com>
2016-03-24 10:31:23 -0700

    added helper for active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def nav_link(link_text, link_path)
+	  class_name = current_page?(link_path) ? 'ui item active' : 'ui item'
+	  link_to(link_text, link_path, class: class_name)
+	end
 end
