31cdc7700a00a9e702c0fcce35671b6b5e5694fe
Agnė Klimaitė <agnaite@me.com>
2016-03-25 16:43:35 -0700

    cleaning up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
 	def welcome
+		@title = 'home'
 	end
 
 	def about
+		@title = 'about'
 	end
 
 	def portfolio
+		@title = 'portfolio'
 	end
 	
 	def contact
+		@title = 'contact'
 	end
 end
