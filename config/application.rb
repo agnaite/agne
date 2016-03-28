9abb5cc63a7fe1af1c2de7582ed75b2359d59dbe
Agnė Klimaitė <agnaite@me.com>
2016-03-27 20:29:59 -0700

    removed font stuff

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,8 +23,5 @@ module Portfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w( .svg .eot .otf .woff2 .woff .ttf)
   end
 end
